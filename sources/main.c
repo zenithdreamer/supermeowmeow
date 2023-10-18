e9e43f2c2b4f2d53bccef357c5f860f2efdcad1f
zenithdreamer <49715907+zenithdreamer@users.noreply.github.com>
2023-10-18 20:21:32 +0900

    "Scene" like system

--- sources/main.c
+++ sources/main.c
@@ -5,9 +5,11 @@
 #define baseScreenWidth 1920
 #define baseScreenHeight 1080
 
+const float baseX = -(baseScreenWidth / 2);
+const float baseY = -(baseScreenHeight / 2);
 const float targetAspectRatio = (float)baseScreenWidth / (float)baseScreenHeight;
 
-void setRuntimeResolution(Camera2D *camera, int screenWidth, int screenHeight)
+void SetRuntimeResolution(Camera2D *camera, int screenWidth, int screenHeight)
 {
     SetWindowSize(screenWidth, screenHeight);
     float currentAspectRatio = (float)screenWidth / (float)screenHeight;
@@ -18,70 +20,53 @@ void setRuntimeResolution(Camera2D *camera, int screenWidth, int screenHeight)
     camera->offset.y = screenHeight / 2.0f;
 }
 
-int main()
+void WindowUpdate(Camera2D* camera)
 {
-    float baseX = - (baseScreenWidth / 2);
-    float baseY = - (baseScreenHeight / 2);
+    if (IsWindowResized())
+    {
+        int screenWidth = GetScreenWidth();
+        int screenHeight = GetScreenHeight();
 
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+        float currentAspectRatio = (float)screenWidth / (float)screenHeight;
+        float scale = (currentAspectRatio > targetAspectRatio) ? (float)screenHeight / baseScreenHeight : (float)screenWidth / baseScreenWidth;
 
-    InitWindow(baseScreenWidth, baseScreenHeight, "SuperMeowMeow");
+        camera->zoom = scale;
+        camera->offset.x = screenWidth / 2.0f;
+        camera->offset.y = screenHeight / 2.0f;
+    }
 
-    // Center of screen
-    SetWindowPosition(200, 200);
+    // Alt - Enter fullscreen
+    if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_ENTER))
+    {
+        SetRuntimeResolution(camera, baseScreenWidth, baseScreenHeight);
+        ToggleFullscreen();
+    }
+}
 
-    Camera2D camera = { 0 };
-    camera.target = (Vector2){ 0.0f, 0.0f };
-    camera.offset = (Vector2){ baseScreenWidth / 2.0f, baseScreenHeight / 2.0f };
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
+void GameUpdate(Camera2D* camera)
+{
+    Texture2D backgroundTexture = LoadTexture(ASSETS_PATH"Background.png");
 
     float circleRadius = 30.0f;
     Vector2 circlePosition = { 100, 50 };
     bool isDragging = false;
 
-    SetTargetFPS(60);
-
-    Texture2D backgroundTexture = LoadTexture(ASSETS_PATH"Background.png");
-
-    setRuntimeResolution(&camera, 1280, 720);
-
-    float willGoFullscreenNextFrame = true;
-
     while (!WindowShouldClose())
     {
-
-        if (IsWindowResized())
-        {
-            int screenWidth = GetScreenWidth();
-            int screenHeight = GetScreenHeight();
-
-            float currentAspectRatio = (float)screenWidth / (float)screenHeight;
-            float scale = (currentAspectRatio > targetAspectRatio) ? (float)screenHeight / baseScreenHeight : (float)screenWidth / baseScreenWidth;
-
-            camera.zoom = scale;
-            camera.offset.x = screenWidth / 2.0f;
-            camera.offset.y = screenHeight / 2.0f;
-        }
-
-        // Alt - Enter fullscreen
-        if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_ENTER))
-		{
-            setRuntimeResolution(&camera, baseScreenWidth, baseScreenHeight);
-            ToggleFullscreen();
-		}
-
-
+        WindowUpdate(camera);
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
-            Vector2 mousePos = GetMousePosition();
+            //Vector2 mousePos = GetMousePosition();
 
             // Convert mouse position to world space, taking into account camera zoom
-            mousePos.x = (mousePos.x - GetScreenWidth() / 2.0f) / camera.zoom + camera.target.x;
-            mousePos.y = (mousePos.y - GetScreenHeight() / 2.0f) / camera.zoom + camera.target.y;
+            //mousePos.x = (mousePos.x - GetScreenWidth() / 2.0f) / camera->zoom + camera->target.x;
+            //mousePos.y = (mousePos.y - GetScreenHeight() / 2.0f) / camera->zoom + camera->target.y;
+
+            // Convert mouse position from screen space to world space
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), *camera);
 
             // Calculate the vector from the circle's center to the mouse position
-            Vector2 circleToMouse = Vector2Subtract(mousePos, circlePosition);
+            Vector2 circleToMouse = Vector2Subtract(mouseWorldPos, circlePosition);
 
             // Check if the mouse click is within the circle (distance <= circleRadius)
             if (Vector2Length(circleToMouse) <= circleRadius)
@@ -98,9 +83,10 @@ int main()
         {
             // Update circle position during dragging, considering camera zoom
             circlePosition = GetMousePosition();
-            circlePosition.x = (circlePosition.x - GetScreenWidth() / 2.0f) / camera.zoom + camera.target.x;
-            circlePosition.y = (circlePosition.y - GetScreenHeight() / 2.0f) / camera.zoom + camera.target.y;
+            circlePosition.x = (circlePosition.x - GetScreenWidth() / 2.0f) / camera->zoom + camera->target.x;
+            circlePosition.y = (circlePosition.y - GetScreenHeight() / 2.0f) / camera->zoom + camera->target.y;
         }
+
 
         // Draw
 
@@ -108,7 +94,7 @@ int main()
         ClearBackground(BLACK);
 
 
-        BeginMode2D(camera);
+        BeginMode2D(*camera);
 
 
         int imageWidth = backgroundTexture.width;
@@ -131,8 +117,107 @@ int main()
 
         EndDrawing();
     }
-
     CloseWindow();
 
+}
+
+void MainMenuUpdate(Camera2D* camera)
+{
+    Texture2D backgroundTexture = LoadTexture(ASSETS_PATH"Background.png");
+
+    // Button positions and dimensions
+    Rectangle startButtonRect = { baseX + 100, baseY + 30, 200, 50 };
+    Rectangle optionsButtonRect = { baseX + 100, baseY + 120, 200, 50 };
+    Rectangle exitButtonRect = { baseX + 100, baseY + 320, 200, 50 };
+
+    while (!WindowShouldClose())
+    {
+        WindowUpdate(camera);
+
+        // Convert mouse position from screen space to world space
+        Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), *camera);
+
+        bool isStartButtonHovered = CheckCollisionPointRec(mouseWorldPos, startButtonRect);
+        bool isOptionsButtonHovered = CheckCollisionPointRec(mouseWorldPos, optionsButtonRect);
+        bool isExitButtonHovered = CheckCollisionPointRec(mouseWorldPos, exitButtonRect);
+
+        // Check for button clicks
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            if (isStartButtonHovered)
+            {
+                GameUpdate(camera);
+                break;
+            }
+            else if (isOptionsButtonHovered)
+            {
+                // Handle Options button click (you can add your logic here)
+                // For now, we'll just print a message
+                printf("Options button clicked!\n");
+            }
+            else if (isExitButtonHovered)
+            {
+                // Handle Exit Game button click (you can add your logic here)
+                // For now, we'll just close the window
+                CloseWindow();
+            }
+        }
+
+        // Draw
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+
+        BeginMode2D(*camera);
+
+
+        int imageWidth = backgroundTexture.width;
+        int imageHeight = backgroundTexture.height;
+
+        float scaleX = (float)baseScreenWidth / imageWidth;
+        float scaleY = (float)baseScreenHeight / imageHeight;
+
+        // Draw the background with the scaled dimensions
+        DrawTextureEx(backgroundTexture, (Vector2) { baseX, baseY }, 0.0f, fmax(scaleX, scaleY), WHITE);
+
+        // Draw buttons
+        DrawRectangleRec(startButtonRect, isStartButtonHovered ? SKYBLUE : BLUE);
+        DrawRectangleRec(optionsButtonRect, isOptionsButtonHovered ? SKYBLUE : BLUE);
+        DrawRectangleRec(exitButtonRect, isExitButtonHovered ? SKYBLUE : BLUE);
+
+        // Draw button labels
+        DrawText("Start Game", (int)(startButtonRect.x + 40), (int)(startButtonRect.y + 15), 20, WHITE);
+        DrawText("Options", (int)(optionsButtonRect.x + 60), (int)(optionsButtonRect.y + 15), 20, WHITE);
+        DrawText("Exit Game", (int)(exitButtonRect.x + 40), (int)(exitButtonRect.y + 15), 20, WHITE);
+
+
+        EndMode2D();
+        EndDrawing();
+    }
+    CloseWindow();
+}
+
+int main()
+{
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
+    InitWindow(baseScreenWidth, baseScreenHeight, "SuperMeowMeow");
+
+    // Center of screen
+    SetWindowPosition(200, 200);
+
+    Camera2D camera = { 0 };
+    camera.target = (Vector2){ 0.0f, 0.0f };
+    camera.offset = (Vector2){ baseScreenWidth / 2.0f, baseScreenHeight / 2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    SetTargetFPS(60);
+    SetRuntimeResolution(&camera, 1280, 720);
+
+    float willGoFullscreenNextFrame = true;
+
+    MainMenuUpdate(&camera);
     return 0;
 }
