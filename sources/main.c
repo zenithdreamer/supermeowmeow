6a1bb3b7cb8458ef24c45b82408cbd4e3aa098c8
zenithdreamer <49715907+zenithdreamer@users.noreply.github.com>
2023-10-18 23:04:25 +0900

    Options menu

--- sources/main.c
+++ sources/main.c
@@ -8,6 +8,36 @@
 const float baseX = -(baseScreenWidth / 2);
 const float baseY = -(baseScreenHeight / 2);
 const float targetAspectRatio = (float)baseScreenWidth / (float)baseScreenHeight;
+const int targetFps = 300;
+
+Texture2D backgroundTexture;
+
+typedef enum {
+    EASY,
+    MEDIUM,
+    HARD
+} Difficulty;
+
+static inline char* StringFromDifficultyEnum(enum Difficulty difficulty)
+{
+    static const char* strings[] = { "Easy", "Medium", "Hard" };
+    return strings[difficulty];
+}
+
+typedef struct Resolution {
+	int x;
+	int y;
+} Resolution;
+
+// Game Options in struct
+typedef struct GameOptions {
+    Resolution resolution;
+	int targetFps;
+	bool fullscreen;
+    Difficulty difficulty;
+} GameOptions;
+
+GameOptions *options;
 
 void SetRuntimeResolution(Camera2D *camera, int screenWidth, int screenHeight)
 {
@@ -18,7 +48,14 @@ void SetRuntimeResolution(Camera2D *camera, int screenWidth, int screenHeight)
     camera->zoom = scale;
     camera->offset.x = screenWidth / 2.0f;
     camera->offset.y = screenHeight / 2.0f;
+
+    options->resolution.x = screenWidth;
+    options->resolution.y = screenHeight;
 }
+
+// Function prototype
+void MainMenuUpdate(Camera2D* camera);
+void OptionsUpdate(Camera2D* camera);
 
 void WindowUpdate(Camera2D* camera)
 {
@@ -33,20 +70,176 @@ void WindowUpdate(Camera2D* camera)
         camera->zoom = scale;
         camera->offset.x = screenWidth / 2.0f;
         camera->offset.y = screenHeight / 2.0f;
+
+        options->resolution.x = screenWidth;
+        options->resolution.y = screenHeight;
     }
 
     // Alt - Enter fullscreen
-    if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_ENTER))
+    if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_ENTER) || IsKeyDown(KEY_RIGHT_ALT) && IsKeyPressed(KEY_ENTER))
     {
         SetRuntimeResolution(camera, baseScreenWidth, baseScreenHeight);
         ToggleFullscreen();
     }
 }
 
-void GameUpdate(Camera2D* camera)
+void UnloadGlobalTextures()
 {
-    Texture2D backgroundTexture = LoadTexture(ASSETS_PATH"Background.png");
+    UnloadTexture(backgroundTexture);
+}
 
+void ExitApplication()
+{
+    UnloadGlobalTextures();
+    
+    // Exit with exit code 0
+    exit(0);
+}
+
+
+void OptionsUpdate(Camera2D* camera)
+{
+    // Define rectangles for UI elements (e.g., buttons, dropdowns)
+    Rectangle difficultyRect = { baseX + 100, baseY + 30, 200, 50 };
+    Rectangle resolutionRect = { baseX + 100, baseY + 120, 200, 50 };
+    Rectangle fpsRect = { baseX + 100, baseY + 220, 200, 50 };
+    Rectangle fullscreenRect = { baseX + 100, baseY + 320, 200, 50 };
+    Rectangle backRect = { baseX + 100, baseY + 480, 200, 50 };
+
+    bool firstRender = true;
+
+    while (!WindowShouldClose())
+    {
+        WindowUpdate(camera);
+
+        // Convert mouse position from screen space to world space
+        Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), *camera);
+
+        bool isDifficultySelected = CheckCollisionPointRec(mouseWorldPos, difficultyRect);
+        bool isResolutionSelected = CheckCollisionPointRec(mouseWorldPos, resolutionRect);
+        bool isFpsSelected = CheckCollisionPointRec(mouseWorldPos, fpsRect);
+        bool isFullscreenToggled = CheckCollisionPointRec(mouseWorldPos, fullscreenRect);
+        bool isBackSelected = CheckCollisionPointRec(mouseWorldPos, backRect);
+
+        // Handle user input
+        if (!firstRender && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            if (isDifficultySelected) {
+                // Toggle difficulty (cycle through the options)
+                options->difficulty = (options->difficulty + 1) % 3;
+            }
+            else if (isResolutionSelected) {
+                // 1280x720, 1920x1080
+                if (options->resolution.x == 1920) {
+                    options->resolution.x = 1280;
+                    options->resolution.y = 720;
+                }
+                else if (options->resolution.x == 1280) {
+					options->resolution.x = 1920;
+					options->resolution.y = 1080;
+				}
+                else
+                {
+                    // Custom resize resolution
+                    // Change it to closest resolution, either 1280x720, 1920x1080
+                    if (options->resolution.x > 1600) {
+						options->resolution.x = 1920;
+						options->resolution.y = 1080;
+					}
+					else {
+						options->resolution.x = 1280;
+						options->resolution.y = 720;
+					}
+
+                }
+                SetRuntimeResolution(camera, options->resolution.x, options->resolution.y);
+            }
+            else if (isFpsSelected) {
+                // Change target FPS 30, 60, 120, 144, 240, Basically Unlimited (1000)
+                if (options->targetFps == 30) {
+					options->targetFps = 60;
+				}
+				else if (options->targetFps == 60) {
+					options->targetFps = 120;
+				}
+				else if (options->targetFps == 120) {
+					options->targetFps = 144;
+				}
+				else if (options->targetFps == 144) {
+					options->targetFps = 240;
+				}
+				else if (options->targetFps == 240) {
+					options->targetFps = 1000;
+				}
+				else if (options->targetFps == 1000) {
+					options->targetFps = 30;
+				}
+            }
+            else if (isFullscreenToggled) {
+                // Toggle fullscreen
+                options->fullscreen = !options->fullscreen;
+                SetRuntimeResolution(camera, options->resolution.x, options->resolution.y);
+                ToggleFullscreen();
+            }
+            else if (isBackSelected) {
+				// Go back to main menu
+				MainMenuUpdate(camera);
+				break;
+			}
+        }
+
+        if (firstRender)
+            firstRender = false;
+
+        // Draw
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+
+        BeginMode2D(*camera);
+
+
+        int imageWidth = backgroundTexture.width;
+        int imageHeight = backgroundTexture.height;
+
+        float scaleX = (float)baseScreenWidth / imageWidth;
+        float scaleY = (float)baseScreenHeight / imageHeight;
+
+        // Draw the background with the scaled dimensions
+        DrawTextureEx(backgroundTexture, (Vector2) { baseX, baseY }, 0.0f, fmax(scaleX, scaleY), WHITE);
+
+        // Draw UI elements
+        DrawRectangleRec(difficultyRect, isDifficultySelected ? SKYBLUE : BLUE);
+        DrawRectangleRec(resolutionRect, isResolutionSelected ? SKYBLUE : BLUE);
+        DrawRectangleRec(fpsRect, isFpsSelected ? SKYBLUE : BLUE);
+        DrawRectangleRec(fullscreenRect, isFullscreenToggled ? SKYBLUE : BLUE);
+        DrawRectangleRec(backRect, isBackSelected ? SKYBLUE : BLUE);
+
+        char difficultyText[50];
+        sprintf(difficultyText, "Difficulty: %s", StringFromDifficultyEnum(options->difficulty));
+
+        char targetFpsText[20];
+        sprintf(targetFpsText, "Fps: %d", options->targetFps);
+
+        char resolutionText[20];
+        sprintf(resolutionText, "%dx%d", options->resolution.x, options->resolution.y);
+
+        DrawText(difficultyText, (int)(difficultyRect.x + 40), (int)(difficultyRect.y + 15), 20, WHITE);
+        DrawText(resolutionText, (int)(resolutionRect.x + 40), (int)(resolutionRect.y + 15), 20, WHITE);
+        DrawText(targetFpsText, (int)(fpsRect.x + 40), (int)(fpsRect.y + 15), 20, WHITE);
+        DrawText("Fullscreen", (int)(fullscreenRect.x + 40), (int)(fullscreenRect.y + 15), 20, WHITE);
+        DrawText("Back", (int)(backRect.x + 40), (int)(backRect.y + 15), 20, WHITE);
+
+        EndMode2D();
+        EndDrawing();
+    }
+
+    UnloadTexture(backgroundTexture);
+    CloseWindow();
+}
+
+void GameUpdate(Camera2D *camera)
+{
     float circleRadius = 30.0f;
     Vector2 circlePosition = { 100, 50 };
     bool isDragging = false;
@@ -117,14 +310,14 @@ void GameUpdate(Camera2D* camera)
 
         EndDrawing();
     }
+
+    UnloadTexture(backgroundTexture);
     CloseWindow();
 
 }
 
-void MainMenuUpdate(Camera2D* camera)
+void MainMenuUpdate(Camera2D *camera)
 {
-    Texture2D backgroundTexture = LoadTexture(ASSETS_PATH"Background.png");
-
     // Button positions and dimensions
     Rectangle startButtonRect = { baseX + 100, baseY + 30, 200, 50 };
     Rectangle optionsButtonRect = { baseX + 100, baseY + 120, 200, 50 };
@@ -151,15 +344,12 @@ void MainMenuUpdate(Camera2D* camera)
             }
             else if (isOptionsButtonHovered)
             {
-                // Handle Options button click (you can add your logic here)
-                // For now, we'll just print a message
-                printf("Options button clicked!\n");
+                OptionsUpdate(camera);
+                break;
             }
             else if (isExitButtonHovered)
             {
-                // Handle Exit Game button click (you can add your logic here)
-                // For now, we'll just close the window
-                CloseWindow();
+                ExitApplication();
             }
         }
 
@@ -195,6 +385,8 @@ void MainMenuUpdate(Camera2D* camera)
         EndMode2D();
         EndDrawing();
     }
+
+    UnloadTexture(backgroundTexture);
     CloseWindow();
 }
 
@@ -213,10 +405,20 @@ int main()
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
 
-    SetTargetFPS(60);
-    SetRuntimeResolution(&camera, 1280, 720);
+    GameOptions _options;
 
-    float willGoFullscreenNextFrame = true;
+    _options.difficulty = MEDIUM;
+    _options.resolution = (Resolution){ 1280, 720 };
+    _options.targetFps = 240;
+    _options.fullscreen = false;  // Default to windowed mode
+
+    options = &_options;
+
+
+    SetTargetFPS(options->targetFps);
+    SetRuntimeResolution(&camera, options->resolution.x, options->resolution.y);
+
+    backgroundTexture = LoadTexture(ASSETS_PATH"Background.png");
 
     MainMenuUpdate(&camera);
     return 0;
