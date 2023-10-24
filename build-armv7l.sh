c2cbf95c18147043fe612642a82a279c1c0c65d9
zenithdreamer <49715907+zenithdreamer@users.noreply.github.com>
2023-10-24 17:08:35 +0900

    build: Fix aarch64

--- build-armv7l.sh
+++ build-armv7l.sh
@@ -1,7 +1,9 @@
 export MESA_GLSL_VERSION_OVERRIDE=330
 export MESA_GL_VERSION_OVERRIDE=3.3
+export PLATFORM=PLATFORM_DESKTOP
+export GRAPHICS=GRAPHICS_API_OPENGL_21
 # sudo apt install xorg-dev -y
 
-cmake -S . -B build
+cmake -S . -B build -B
 cd build
 make
