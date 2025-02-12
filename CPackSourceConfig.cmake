2eecda09fda4844b85b234cb927eccd6099d4726
zenithdreamer <49715907+zenithdreamer@users.noreply.github.com>
2025-02-12 04:51:50 +0000

    Deploying to gh-pages from @ zenithdreamer/SuperMeowMeow 🚀

--- CPackSourceConfig.cmake
+++ CPackSourceConfig.cmake
@@ -18,7 +18,7 @@ set(CPACK_DEBIAN_PACKAGE_DEPENDS "libatomic1, libc6, libglfw3, libglu1-mesa | li
 set(CPACK_DEBIAN_PACKAGE_NAME "libraylib-dev")
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS "OFF")
 set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "/usr/local/share/cmake-3.31/Templates/CPack.GenericDescription.txt")
-set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "my_raylib_game built using CMake")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "index built using CMake")
 set(CPACK_GENERATOR "TBZ2;TGZ;TXZ;TZ")
 set(CPACK_IGNORE_FILES "/CVS/;/\\.svn/;/\\.bzr/;/\\.hg/;/\\.git/;\\.swp\$;\\.#;/#")
 set(CPACK_INNOSETUP_ARCHITECTURE "x86")
